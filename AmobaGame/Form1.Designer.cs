04d66ce9899e67de500899fae1fc0ced38b58609
Adesz05 <kerekadamtamas@turr.hu>
2022-10-12 09:54:56 +0200

    Cica

--- AmobaGame/Form1.Designer.cs
+++ AmobaGame/Form1.Designer.cs
@@ -29,10 +29,19 @@ namespace AmobaGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Játék";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
