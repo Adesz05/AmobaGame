f396ecb37a603266419bdf3d3feb81ca8cd14685
Adesz05 <kerekadamtamas@turr.hu>
2022-10-17 10:31:48 +0200

    c

--- AmobaGame/Jatekter.cs
+++ AmobaGame/Jatekter.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AmobaGame
@@ -82,7 +76,7 @@ namespace AmobaGame
                     }
                 }
             }
-            
+
 
 
 
